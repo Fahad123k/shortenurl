0e6c88600a773db4895b483f7fed98064c6bba00
fahad123k <ahmad.nishu08@gmail.com>
2025-09-19 21:00:20 +0530

    mirnor mistake on frontend url cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: ["http://localhost:5173/"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
 }))
