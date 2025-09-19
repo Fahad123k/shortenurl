48821394d7e35a33eb5756b24ecaf3b4a12a01f8
fahad123k <ahmad.nishu08@gmail.com>
2025-09-19 20:58:24 +0530

    add methods to cors

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: ["http://localhost:5173/"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
 }))
 
