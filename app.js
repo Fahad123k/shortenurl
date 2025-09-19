cd5c99bdf4b710ea94851b74c00dad3832ceb630
fahad123k <ahmad.nishu08@gmail.com>
2025-09-19 21:27:43 +0530

    temporary remove credentials

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(cors({
     origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
+    // credentials: true,
 }))
 
 app.use(securityMiddleware);
